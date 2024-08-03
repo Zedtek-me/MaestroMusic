553520adb732bb5048d488c1b8df15093612188d
Zechariah <zechariahadebayo42@gmail.com>
2024-08-03 23:07:23 +0100

    setup continuation-2

--- apps/musicapp/apps.py
+++ apps/musicapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MusicappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'musicapp'
+    name = 'apps.musicapp'
--- src/settings.py
+++ src/settings.py
@@ -14,13 +14,15 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # project apps
-    'musicapp',
+    'apps.musicapp',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
